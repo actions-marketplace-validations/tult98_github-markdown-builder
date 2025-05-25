259edce8bf27ddb2a21c694bfed994795e7af417
Tu Le <tu.le@employmenthero.com>
2025-05-25 21:22:51 +0700

    fix: correct setup for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['./.github/linters/.eslintrc.yml'],
+  overrides: [
+    {
+      files: ['.eslintrc.js'],
+      parserOptions: { project: null }
+    },
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-floating-promises': 'off'
+      }
+    }
+  ]
+};
